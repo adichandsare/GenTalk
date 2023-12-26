97204f689739da636e2964f1bcccb2f5c73f13d5
aditya <apchandsare@gmail.com>
2023-12-26 19:21:52 +0530

    Initial commit

---
+++ frontend/src/ChatPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId='9584d974-720f-4f87-9f2e-397e329775aa'
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
